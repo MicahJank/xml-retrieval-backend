dcfda890f03dfb303502b9c18fd1724714401a68
Micah <micahjank@gmail.com>
2020-10-20 15:15:17 -0500

    got server running

---
+++ api/configureMiddleware.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+module.exports = server => {
+    server.use(cors());
+    server.use(express.json());
+    server.use(helmet());
+
+}
